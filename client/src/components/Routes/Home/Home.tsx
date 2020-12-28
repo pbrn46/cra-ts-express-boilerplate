14398e96a84169fbd363fb1afc082695d8627106
Boris Wong <bobobwong@gmail.com>
2020-12-28 09:20:01 -0800

    Packages updated.

--- client/src/components/Routes/Home/Home.tsx
+++ client/src/components/Routes/Home/Home.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container } from '@material-ui/core'
+import axios from "axios"
 
 export default function Home() {
+  const [serverMessage, setServerMesage] = useState("")
+  const [serverError, setServerError] = useState<string | null>(null)
+  useEffect(() => {
+    async function fetch() {
+      try {
+        setServerError(null)
+        const res = await axios.get("/api/")
+        setServerMesage(res.data)
+      } catch (err) {
+        setServerError("Server error.")
+      }
+    }
+    fetch()
+  }, [])
   return <Container>
-    Hello!
+    <div>{serverError}</div>
+    <div>{serverMessage}</div>
   </Container>
 }
--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,7 +1,12 @@
 import path from 'path'
 import express, { Router } from 'express'
 
+import apiRoute from './api'
+
+
 const router = Router()
+
+router.use("/api", apiRoute)
 
 export const REACT_BUILD_PATH = path.normalize(path.join(__dirname, '..', '..', '..', 'client', 'build'))
 router.use(express.static(REACT_BUILD_PATH));
